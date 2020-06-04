432b8ee96a5c69d4b1d0bbdeefeaa27500b86eee
korey ko <koreykendrick1@gmail.com>
2020-06-04 13:23:12 +0000

    Automatically backed up by Learn

---
+++ lib/school.rb
@@ -0,0 +1,19 @@
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student, level)
+    roster[level] ||= []
+    roster[level] << student
+  end 
+  
+  def grade(level)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+      end 
+    end 
