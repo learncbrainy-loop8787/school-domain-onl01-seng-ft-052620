c356a862e9158ae688eb7bbb5feb8a27210bfc13
korey ko <koreykendrick1@gmail.com>
2020-06-04 13:24:15 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,14 +6,5 @@ class School
     @roster = {}
   end 
   
-  def add_student(student, level)
-    roster[level] ||= []
-    roster[level] << student
-  end 
   
-  def grade(level)
-    roster.detect do |x, y| 
-      if x == level
-        return y 
-      end 
     end 
